066c1e51e8c657363376bddd69890b80e67fbb92
Aishwary200 <amritakeshri3059@gmail.com>
2020-12-12 23:19:50 +0530

    project 45

--- bike.js
+++ bike.js
@@ -3,16 +3,15 @@ class Bike {
         var options = {
             restitution: 0.2
         }
-        this.body = Bodies.rectangle(x, y, 50, options)
+        this.body = Bodies.circle(x, y, 50)
         this.radius = 50;
-
         this.image = loadImage("images/bike.png")
         World.add(world, this.body)
     }
     display() {
         var angle = this.body.angle
         push();
-        translate(this.body.position.x, this.body.position.y)        
+        translate(this.body.position.x, this.body.position.y)
         rotate(angle)
         imageMode(CENTER)
         image(this.image, 0, 0, this.radius * 2, this.radius * 2)
--- sketch.js
+++ sketch.js
@@ -5,7 +5,9 @@ const Bodies = Matter.Bodies
 var engine, world;
 var bike, bikeImg;
 var ramp;
-var backImg
+var backImg;
+var ground;
+
 function preload() {
   backImg = loadImage("images/background.jpg")
   //bikeImg = loadImage("images/bike.png")
@@ -17,32 +19,45 @@ function setup() {
   //createSprite(400, 200, 50, 50);
   bike = new Bike(80, 300)
   ramp = new Ramp(600, 300, 100)
+  ground = new Ground(500, 390, 1000, 20)
   Engine.run(engine)
 }
 
 function draw() {
   background(backImg);
   Engine.update(engine)
-  // if(bike.x>ramp.x){
 
-  // }
-  if (keyDown(RIGHT_ARROW)) {
-    bike.x = bike.x + 10
-  }
-  if (keyDown(LEFT_ARROW)) {
-    bike.x = bike.x - 10
-  }
   detectollision(bike, ramp)
   drawSprites();
   ramp.display();
   bike.display();
+  ground.display();
 }
+
 function detectollision(lbike, lramp) {
   rampBodyPosition = lramp.body.position;
   bikeBodyPosition = lbike.body.position;
   var distance = dist(bikeBodyPosition.x, bikeBodyPosition.y, rampBodyPosition.x, rampBodyPosition.y)
-  if (distance <= lbike.radius + lramp.radius) {
-    bike.y = bike.y - 100
-    bike.x = bike.x + 50
+  if (distance <= lbike.radius + lramp.radius && bike.body.position.y>200) {
+    bike.body.position.y = bike.body.position.y - 10
+    bike.body.position.x = bike.body.position.x + 10
+  }
+}
+function keyPressed() {
+  if (keyCode === RIGHT_ARROW) {
+    bike.body.position.x = bike.body.position.x + 1
+    console.log("hi")
+  }
+  if (keyCode === LEFT_ARROW) {
+    bike.body.position.x = bike.body.position.x - 1
+  }
+  if (keyCode === UP_ARROW) {
+    bike.body.rotate(-10);
+  }
+  if (keyCode === DOWN_ARROW) {
+    bike.body.rotate(10);
+  }
+  if (keyCode === 32) {
+    bike.body.position.y = bike.body.position.y - 10
   }
 }
