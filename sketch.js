f3b7256a2a29786180064d890290497ea190980c
Aishwary200 <amritakeshri3059@gmail.com>
2020-12-19 00:34:44 +0530

    project 46

--- sketch.js
+++ sketch.js
@@ -21,18 +21,21 @@ function setup() {
   world = engine.world
   createCanvas(1000, 400);
   bike = new Bike(80, 300)
-  ground = new Ground(width / 2, 390, 1000, 20)
+  ground = new Ground(500, 390, 10000, 20)
+  ramp = new Ramp(600, 300, 100)
   obstacleGroup = new Group();
-  console.log(frameCount)
+  //console.log(frameCount)
   Engine.run(engine)
 }
 
 function draw() {
   background(backImg);
   Engine.update(engine);
-  if (ground.body.position.x < 0) {
-    ground.body.width += 1000
-  }
+  // if (ground.body.position.x < 0) {
+  //   ground.body.width += 1000
+  // }
+  //ground.body.position.x=bike.body.position.x
+ // console.log(bike.body.velocityX)
 
 
   rampM();
@@ -41,7 +44,9 @@ function draw() {
   ground.display();
   spawnObstacle();
   detectollision(bike, ramp)
+  detectCollision(bike,obstacleGroup)
   drawSprites();
+  console.log(obstacle)
   if (obstacleGroup = ramp.body.position.x) {
     obstacleGroup.velocityX = 0
   }
@@ -56,8 +61,16 @@ function detectollision(lbike, lramp) {
   var distance = dist(bikeBodyPosition.x, bikeBodyPosition.y, rampBodyPosition.x, rampBodyPosition.y)
   if (distance <= lbike.radius + lramp.radius && bike.body.position.y > 200) {
     bike.body.position.y = bike.body.position.y - 1
-    console.log("abc")
     bike.body.position.x = bike.body.position.x + 1
+  }
+}
+function detectCollision(lbike, lobstacle) {
+  obstacleBodyPosition = lobstacle;
+  bikeBodyPosition = lbike.body.position;
+
+  var distance = dist(bikeBodyPosition.x, bikeBodyPosition.y, obstacleBodyPosition.x, obstacleBodyPosition.y)
+  if (distance <= lbike.radius+1000) {
+    bike.body.position.x = bike.body.position.x - 10
   }
 }
 function keyPressed() {
@@ -65,7 +78,6 @@ function keyPressed() {
     bike.body.position.x = bike.body.position.x + 10
     camera.position.x = bike.body.position.x
     //camera.position.y=bike.body.position.y
-    console.log("hi")
   }
   if (keyCode === LEFT_ARROW) {
     bike.body.position.x = bike.body.position.x - 10
@@ -89,12 +101,13 @@ function spawnObstacle() {
     obstacle.addImage(obstacleImg)
     obstacle.velocityX = -2;
     obstacle.scale = 0.2;
+    obstacle.debug=true
     obstacleGroup.add(obstacle);
   }
 
 }
 function rampM() {
-  if (frameCount % 100 === 0) {
+  if (frameCount % 10 === 0) {
     ramp = new Ramp(600, 300, 100)
   }
 }
